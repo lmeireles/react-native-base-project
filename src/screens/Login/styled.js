2200775e80ee88bc66a8eba58a110f01b9d92537
lmeireles <luis.lc93@hotmail.com>
2019-05-18 21:19:52 +0000

    Animation on Login

--- src/screens/Login/styled.js
+++ src/screens/Login/styled.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 import {ViewColumnCenter, Spacer} from "../../components/layout";
+import * as Animatable from 'react-native-animatable';
 
 const StyledImage = styled.Image`
-  width: 150px;
-  height: 150px;
-  min-height: 150px;
+  width: 180px;
+  height: 180px;
+  min-height: 180px;
 `;
+
 export const Logo = props => (
   <ViewColumnCenter center>
-    <StyledImage
+    <AnimatedImage
       {...props}
       resizeMode={'contain'}
+      animation={'bounceIn'}
     />
     <Spacer space={34}/>
   </ViewColumnCenter>
 );
+const AnimatedImage = Animatable.createAnimatableComponent(StyledImage);
